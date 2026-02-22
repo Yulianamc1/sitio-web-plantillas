57de161ff97a524df0baa48558c272e932cfde4b
Yulianamc1 <y6851537@gmail.com>
2026-02-21 22:27:46 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// --- Meta Pixel (PageView) ---
+declare global {
+  interface Window {
+    fbq?: (...args: any[]) => void;
+    _fbq?: any;
+  }
+}
+
+(function () {
+  const w = window;
+  if (w.fbq) return; // evita duplicado
+
+  (function (f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(w, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+
+w.fbq!("init", "1130925549084207");
+w.fbq!("track", "PageView");
+})();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
