55bff116616825c9fa7027a1a341d6e24a7ccbb4
Yulianamc1 <y6851537@gmail.com>
2026-03-06 18:54:16 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// META PIXEL
-(function(f,b,e,v,n,t,s)
-{if((f as any).fbq)return;n=(f as any).fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!(f as any)._fbq)(f as any)._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode!.insertBefore(t,s)})(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-
-(window as any).fbq('init', '2154175588655021');
-(window as any).fbq('track', 'PageView');
-// FIN META PIXEL
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
