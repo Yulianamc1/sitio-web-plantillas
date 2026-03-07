cff8720da880cb5b0a25702cebb2d37881d5adbc
Yulianamc1 <y6851537@gmail.com>
2026-03-07 14:34:19 -0600

    Add files via upload

---
+++ move.cjs
@@ -0,0 +1,5 @@
+{
+  "name": "Kit Papelería Creativa",
+  "description": "Landing page para vender un kit de más de 1700 plantillas editables en Canva para emprendedoras de papelería creativa.",
+  "requestFramePermissions": []
+}
