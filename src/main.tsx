f9a2db628338becb3896a24edae19e3b3aacf737
Yulianamc1 <y6851537@gmail.com>
2026-03-21 21:22:27 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,41 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+// Meta Pixel
+declare global {
+  interface Window {
+    fbq?: (...args: any[]) => void;
+    _fbq?: (...args: any[]) => void;
+  }
+}
+
+if (typeof window !== 'undefined') {
+  !(function (f: any, b: Document, e: string, v: string, n?: any, t?: HTMLScriptElement, s?: Element | null) {
+    if (f.fbq) return;
+    n = f.fbq = function (...args: any[]) {
+      if (n.callMethod) {
+        n.callMethod.apply(n, args);
+      } else {
+        n.queue.push(args);
+      }
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e) as HTMLScriptElement;
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s?.parentNode?.insertBefore(t, s);
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+  window.fbq?.('init', '2154175588655021');
+  window.fbq?.('track', 'PageView');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
