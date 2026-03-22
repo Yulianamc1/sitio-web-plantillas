6647f64ade4b32f7e4ce4f6b72a555b14b149144
Yulianamc1 <y6851537@gmail.com>
2026-03-21 21:49:01 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,30 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const w = window as any;
-
-if (typeof window !== 'undefined') {
-  !(function (f: any, b: Document, e: string, v: string, n?: any, t?: HTMLScriptElement, s?: Element | null) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = true;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e) as HTMLScriptElement;
-    t.async = true;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s?.parentNode?.insertBefore(t, s);
-  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-  w.fbq('init', '2154175588655021');
-  w.fbq('track', 'PageView');
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
